0381850e8222788ce8f48e8204e00ce8c7b90c7c
F.I.J.I <37077096+FIJI16@users.noreply.github.com>
2021-09-12 01:14:20 +0300

    Update st-delete-digit.js
    Solution for task

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit( n ) {
+  let massive = [];
+  let true__massive = [];
+  let fake__massive = [];
+  let string__num = String(n);
+  let string__result = "";
+  let search__num = "";
+  
+  for( let i = 0; i < string__num.length; i++) {
+      massive.push( string__num[i] );
+  }
+
+  fake__massive = true__massive.concat(massive);
+
+  fake__massive = fake__massive.sort().reverse();
+  search__num = fake__massive[fake__massive.length - 1];
+
+  for(let k = 0; k < massive.length; k++) {
+
+      if(massive[k] == search__num) {
+          massive.splice(k, 1);
+          break;
+      }
+
+  }
+
+  for(let j = 0; j < massive.length; j++) {
+
+      string__result += massive[j];
+  }
+
+  return Number(string__result);
 }
